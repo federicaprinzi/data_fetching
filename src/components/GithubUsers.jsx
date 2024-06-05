2dcc5976bcd15c6afa8c4ce06d762ac4578999f6
Federica <fedecprinzi@gmail.com>
2024-06-05 23:09:13 +0200

    update_with_customhooks_ex

--- src/components/GithubUsers.jsx
+++ src/components/GithubUsers.jsx
@@ -1,31 +1,7 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useGithubUser } from "../hooks/useGithubUser";
 
 export function GetGithubUser({ username }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  async function fetchGithubUser(username) {
-    setLoading(true);
-    setError(null);
-
-    try {
-      const response = await fetch(`https://api.github.com/users/${username}`);
-      const json = await response.json();
-      setData(json);
-    } catch (error) {
-      setError(error);
-      setData(null);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    fetchGithubUser(username);
-  }, [username]);
-
+  const { data, loading, error } = useGithubUser(username);
   return (
     <div>
       {loading && <h1>Loading...</h1>}
